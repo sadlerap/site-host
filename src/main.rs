2c14c6d3561f3beacdb4ec387003531eca7231c8
Andy Sadler <ansadler@redhat.com>
2022-03-28 12:31:18 -0500

    Implement initial web server
    Signed-off-by: Andy Sadler <ansadler@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{extract::Path, response::IntoResponse, routing::get, Router, Server};
+use std::{error::Error, net::SocketAddr};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+    run_server().await
+}
+
+async fn run_server() -> Result<(), Box<dyn Error>> {
+    let addr: SocketAddr = "0.0.0.0:8080".parse()?;
+    let app = Router::new()
+        .route("/*path", get(serve_path));
+
+    println!("Listening on http://{}", addr);
+
+    Server::bind(&addr).serve(app.into_make_service()).await?;
+
+    Ok(())
+}
+
+async fn serve_path(Path(path): Path<String>) -> impl IntoResponse {
+    format!("Hello world!  Wanting to fetch {}\n", path)
 }
